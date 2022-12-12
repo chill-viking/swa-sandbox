75d3e7ca39816e32117eb4908a6a31423e7e6efc
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-12 23:30:35 +0200

    core(workflow): add test workflow

--- swa-sandbox-ui/apps/swa-sandbox-ui/jest.config.ts
+++ swa-sandbox-ui/apps/swa-sandbox-ui/jest.config.ts
@@ -10,6 +10,7 @@ export default {
     },
   },
   coverageDirectory: '../../coverage/apps/swa-sandbox-ui',
+  coverageReporters: ['lcov'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
