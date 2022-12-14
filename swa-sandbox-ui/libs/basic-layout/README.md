6b2958c1b85602269779c00bd5352ede95946c52
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-14 12:54:56 +0200

    feat: add basic-layout lib

---
+++ swa-sandbox-ui/libs/basic-layout/README.md
@@ -0,0 +1,7 @@
+# basic-layout
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test basic-layout` to execute the unit tests.
---
+++ swa-sandbox-ui/libs/basic-layout/src/lib/basic-layout/basic-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swa-sandbox-basic-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './basic-layout.component.html',
+  styleUrls: ['./basic-layout.component.css'],
+})
+export class BasicLayoutComponent {}
