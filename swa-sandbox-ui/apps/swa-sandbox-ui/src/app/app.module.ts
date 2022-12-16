674435db168561943ca7f4bc21101f0125cbe2ea
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-16 10:18:35 +0200

    feat: update basic-layout with style
    And update app to use layout

--- swa-sandbox-ui/apps/swa-sandbox-ui/src/app/app.module.ts
+++ swa-sandbox-ui/apps/swa-sandbox-ui/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BasicLayoutComponent } from "@swa-sandbox/basic-layout";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BasicLayoutComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
